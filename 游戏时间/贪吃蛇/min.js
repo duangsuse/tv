5dc9d4a22fcee42943d1d01d2b705922ee69dee8
duangsuse <fedora-opensuse@outlook.com>
2021-09-30 22:50:59 +0800

    蛇:支持头尾贴图,重构击键换向

--- 游戏时间/贪吃蛇/min.js
+++ 游戏时间/贪吃蛇/min.js
@@ -1,20 +1,21 @@
 const {random,floor,abs}=Math,ss=s=>s.split(" "),nA=(n,f)=>Array(n).fill(0).map(f),
 rand=n=>floor(random()*n)//随机<n
-isZ=(i,N)=>i==0||i==N//上,左双墙面
+isZ=(i,N)=>i==0||i==N,//上,左双墙面
+清空=(a,f)=>{ let i=0,N=a.length,nF=f.length; a=a.splice(0,N);for(;i<N;i+=nF)f(...a.slice(i,i+nF)) } //仅贴图方块
 
-var RL
-蛇=(n,m,阵)=>{ var p,d=1,ps,  d新=1,c, 选=n=>5+rand(n-5)
+var RL,ps
+蛇=(n,m,阵)=>{ var p,d=1,  d新=[],f=d1=>{if(d+d1)d新.push(d1)},c, 选=n=>5+rand(n-5)
   初=()=>{ 阵.fill(0); p=选(n)*m+选(m);ps=[p]; 新点(3)//清空,p,苹果
     for(let p=0, i=0,j; i<n;i++)for(j=0;j<m;j++,p++)if(isZ(i,n-1)||isZ(j,m-1))阵[p]=2 }//墙; a||b 的可取交集.
   新点=k=>{let p; do{p=rand(n*m)}while(阵[p]!=0); 阵[p]=k; }
 
-  if(cmd) (RL=RL||require("repl").start()).input.on("keypress", (_,ev)=>{ let d1=(c=ev.code)? [-m,+m,+1,-1] [c.charCodeAt(1)-0x41]:-d; if(d+d1)d新=d1 });
-  else{window.onkeydown=ev=>{ let d1=[-1,-m,+1,+m] [ev.keyCode-37/*左键*/]; if(d+d1)d新=d1 }
+  if(cmd) (RL=RL||require("repl").start()).input.on("keypress", (_,ev)=>f( (c=ev.code)? [-m,+m,+1,-1] [c.charCodeAt(1)-0x41]:-d) );
+  else{window.onkeydown=ev=>f([-1,-m,+1,+m] [ev.keyCode-37/*左键*/]);
   for(let kev of ss("mouse touch"))
-    触向(window,kev,(x0,y0,x,y,ev)=>{ x-=x0;y-=y0; let q=abs(x)>abs(y), v=q?1:m, d=q?x:y; if(abs(d)>2)ev.preventDefault(); d新=(d<0? -v:v) })
+    触向(window,kev,(x0,y0,x,y,ev)=>{ x-=x0;y-=y0; let q=abs(x)>abs(y), v=q?1:m, d=q?x:y; if(abs(d)>2)ev.preventDefault(); f(d<0? -v:v) })
   }
   步=()=>{
-    d=d新;p=p+d; if((c= 阵[p])!=0&&c<3) 死掉(); else { 
+    清空(d新,v=>d=v);p=p+d; if((c= 阵[p])!=0&&c<3) 死掉(); else { 
       ps.push(p);阵[p]=1; c==3? 新点(c)/*苹*/ : 阵[ps.shift()]=0 }
     绘()
   }
@@ -28,7 +29,7 @@ var RL
 
 doc=this.document, cmd=globalThis.process
 
-gid=0, 配={绘:0, 复活:0, 界:[20,30], 贴文:"　蛇墙果头尾",贴色:ss("gray blue green red #66ccff #aaaaaa")},//grid/字符; 单次,重开,加分,不死; 高宽
+gid=0, 配={绘:0, 复活:0, 界:[20,30], 贴文:"　蛇墙果头尾",贴色:ss("gray #66ccff green red blue #aaaaaa")},//grid/字符; 单次,重开,加分,不死; 高宽
 主循环=f=>{let[n,m]=配.界,N=n*m, a,ae, [init,go]=f(n,m,a=Array(N)),dt=1000/7 //秒格速度 ;可用 n=cmd.stdout.rows 或 URLSearchParams("w=1").get
   if(doc){let e=doc.body
     e.style.cssText=`white-spaces:pre; user-select:none; display:grid;grid-template-columns:repeat(auto-fill,calc(100% / ${m}))`
@@ -42,13 +43,17 @@ gid=0, 配={绘:0, 复活:0, 界:[20,30], 贴文:"　蛇墙果头尾",贴色:ss(
     if(k==0){if(RL)RL.close();return}//v 重开
     gid=setInterval(go,dt); (k==2)?新点(3):init();
   }
-  let 画文=cmd? s=>{so.write("\x1b[H");so.write(s)} : s=>{doc.body.innerText=s}, so=o=cmd?cmd.stdout:0, {贴色,贴文}=配
-  绘=cmd||配.绘==1? ()=>{let p, j=0,s=[]
-    for(p=0;p<N;p++){ s.push(贴文[a[p]]); j++;if(j==m){j=0;s.push("\n")} }
-    画文(s.join(""))
-  } : ()=>{
-    for(let p=0;p<N;p++)ae[p].background=贴色[a[p]]
+  let 画文=cmd? s=>{so.write("\x1b[H");so.write(s)} : s=>{doc.body.innerText=s}, so=o=cmd?cmd.stdout:0, {贴色,贴文}=配, 旧p=[],
+  绘1=()=>清空(旧p, (p,v0)=>a[p]=v0),绘0=()=>{let N=ps.length; if(N==1)return;
+    [ps[N-1],ps[0]].forEach((p,i)=>{ 旧p.push(p,a[p]); a[p]=4+i })//头
   }
+
+  绘=cmd||配.绘==1? ()=>{let p, j=0,s=[]; 绘0()
+    for(p=0;p<N;p++){ s.push(贴文[a[p]]); j++;if(j==m){j=0;s.push("\n")} }
+    画文(s.join("")); 绘1()
+  } : ()=>{绘0()
+    for(let p=0;p<N;p++)ae[p].background=贴色[a[p]]
+  绘1()}
 }
 
 主循环(蛇)
